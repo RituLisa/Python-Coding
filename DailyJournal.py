c97c5e544cf3ca3475aab7c3c80bd790c609a2f1
RituLisa <157664115+RituLisa@users.noreply.github.com>
2024-01-24 20:59:31 -0600

    DailyJournal.py

--- DailyJournal.py
+++ DailyJournal.py
@@ -10,8 +10,12 @@ def WelcomeMessage():
 
 
 def SearchEntry():
-    x = input("Enter the date of the entry you would like to read, (DDMMYY): ")
-    file = 
+    file = open(filename, "r") 
+    f = input("Enter in a key word from an entry/entries you would like to read:\n")
+    for data in file:
+        if f.lower() in data.lower():
+            print (data)
+    file.close()
 def ReadEntry():
     print("Your diary entries are 1")
     file = open(filename,"r")
@@ -33,28 +37,21 @@ while True:
     print("Choose one of the following options  ")
     print("1. Read a previous entry")
     print("2. Write a new entry")
-    print("3. Exit Journal")
+    print ("3. Search an entry") 
+    print("9. Exit Journal")
     
     option = input("Enter your choice: ")
     print()
     if option == "1":
         ReadEntry()
-        break 
+        input("any key to continue:" )
     elif option =="2":
         WriteEntry()
-        break
+        
     elif option == "3":
-        break
+       SearchEntry()
+       input ("enter to continue:") 
+    elif option == "9":
+        break 
     else:
         print("Invalid Option, Please enter 1/2/3")
-
-
-    
-        
-        
-    
-
-    
-    
-
-    
